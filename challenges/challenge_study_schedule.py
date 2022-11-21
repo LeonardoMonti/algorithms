53bc93863d948b4f2df0d6c29c5ba19780eea17e
LeonardoMonti <LeonardoMonti>
2022-11-21 18:59:18 -0300

    feat: create method search algorithm

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,12 @@
 def study_schedule(permanence_period, target_time):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    try:
+        result = None
+        if target_time is not None:
+            result = 0
+            for period in permanence_period:
+                if period[0] <= target_time <= period[1]:
+                    result += 1
+        return result
+
+    except TypeError:
+        return None
