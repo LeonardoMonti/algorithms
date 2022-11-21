5aa5136f7a169b74ce9c8e051f8002df5f768146
LeonardoMonti <LeonardoMonti>
2022-11-21 19:05:12 -0300

    fix:  change complex

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,11 +1,9 @@
 def study_schedule(permanence_period, target_time):
     try:
-        result = None
-        if target_time is not None:
-            result = 0
-            for period in permanence_period:
-                if period[0] <= target_time <= period[1]:
-                    result += 1
+        result = 0
+        for period in permanence_period:
+            if period[0] <= target_time <= period[1]:
+                result += 1
         return result
 
     except TypeError:
